ddcea2b31716ab6a8b327096ba267a71dd8e76cd
Omar Rodriguez <orodriguez.ordl@gmail.com>
2023-11-19 09:49:44 -0600

    skills

---
+++ js/grayscale.js
@@ -0,0 +1,16 @@
+const logos = document.querySelectorAll('.logo');
+
+function randomizeGrayscale() {
+    logos.forEach(logo => {
+        const randomValue = Math.random();
+        if (randomValue < 0.5) {
+            logo.style.filter = 'grayscale(100%)';
+        } else {
+            logo.style.filter = 'grayscale(0%)';
+            logo.style.transform = `translate(${Math.random() * 10}px, ${Math.random() * 10}px`;
+            logo.style.transition = 'transform 5s';
+        }
+    });
+}
+
+setInterval(randomizeGrayscale, 2000);
